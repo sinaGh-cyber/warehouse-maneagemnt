b5ca209d7decb4caf47800e3471b7e59232755f1
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-03-05 00:06:57 +0330

    chang jsx returned

--- src/components/productManagerApp/productManagerApp.jsx
+++ src/components/productManagerApp/productManagerApp.jsx
@@ -1,14 +1,13 @@
 import './productManagerApp.module.scss';
 
 import ProductList from '../productList/ProductList';
-import Filter from '../filter/Filter';
+
 import AddingProductForm from '../addingProductForm/addingProductForm';
 
 const ProductManagerApp = () => {
   return (
     <>
       <AddingProductForm />
-      <Filter />
       <ProductList />
     </>
   );
